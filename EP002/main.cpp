47cd055148769ce935e89ac74bb50a3af0e1ac60
mohamed-linux <mohamedkarassane@gmail.com>
2024-10-12 09:36:48 +0200

    CU-86965u5ad : automation test9

--- EP002/main.cpp
+++ EP002/main.cpp
@@ -7,6 +7,7 @@ T add(T a, T b) {
     return a + b;
 }
 
+
 int main() {
     std::cout << "Hello from C++ with Clake on Linux (std=c++20)" << std::endl;
     std::cout << "The sum is: " << add(7, 5) << std::endl;
